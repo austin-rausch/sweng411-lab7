b2c797914e4af1dc357335730504176ffbfa3de8
Chris Andrejewski <christopher.andrejewski@gmail.com>
2016-10-04 15:09:22 -0400

    things

--- composition/Author.java
+++ composition/Author.java
@@ -1,1 +1,31 @@
-package lab7;
+package composition;
+
+import java.util.ArrayList;
+
+public class Author {
+  private String name;
+  private String citizenship;
+  private ArrayList<Publication> publications;
+
+  public Author(String name, String citizenship) {
+    setName(name);
+    setCitizenship(citizenship);
+    publications = new ArrayList<Publication>();
+  }
+
+  public String getName() {return name;}
+  public void setName(String _name) {name = _name;}
+
+  public String getCitizenship() {return citizenship;}
+  public void setCitizenship(String _citizenship) {citizenship = _citizenship;}
+
+  public void addPublication(Publication ...publications) {
+    for (Publication p : publications) {
+      this.publications.add(p);
+    }
+  }
+
+  public String toString() {
+    return "Author name: "+name+"\nAuthor citizenship: "+citizenship+"\n";
+  }
+}
